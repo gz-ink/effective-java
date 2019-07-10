7e5af58f9e0890b8d5d7f6ba46cc585cc7a561bd
gz-ink <guozhao1992@gmail.com>
2019-07-10 12:51:10 +0800

    [FunctionalInterface] example

---
+++ src/main/java/com/gzink/effectivejava/functionalinterface/FunctionalUtils.java
@@ -0,0 +1,35 @@
+package com.gzink.effectivejava.functionalinterface;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class FunctionalUtils {
+
+
+    public static List<Integer> filter(List<Integer> list, Predicate<Integer> predicate) {
+        List<Integer> result = new ArrayList<>();
+        for (Integer i : list) {
+            if (predicate.test(i)) {
+                result.add(i);
+            }
+        }
+
+        return result;
+    }
+
+    public static List<String> map(List<Integer> list, Function<Integer, String> function) {
+        List<String> result = new ArrayList<>();
+        for (Integer i : list) {
+            result.add(function.apply(i));
+        }
+        return result;
+    }
+
+
+    public static void add(int i, Runnable runnable) {
+        runnable.run();
+    }
+}
---
+++ src/test/java/com/gzink/effectivejava/functionalinterface/FunctionalUtilsTest.java
@@ -0,0 +1,42 @@
+package com.gzink.effectivejava.functionalinterface;
+
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+public class FunctionalUtilsTest {
+
+    @Test
+    public void should_filter_list() {
+        List<Integer> list = Arrays.asList(1, -2, 4, 5);
+        Predicate<Integer> predicate = (Integer a) -> a > 0;
+        List<Integer> result = FunctionalUtils.filter(list, predicate);
+        assertThat(result, is(Arrays.asList(1, 4, 5)));
+    }
+
+    @Test
+    public void should_map_integer_to_string() {
+        List<Integer> list = Arrays.asList(1, 13, 5, 22);
+        Function<Integer, String> function = (Integer a) -> a > 10 ? "L" : "S";
+        List<String> result = FunctionalUtils.map(list, function);
+        assertThat(result, is(Arrays.asList("S", "L", "S", "L")));
+    }
+
+    @Test
+    public void should_runnable_add_to_list() {
+        List<Integer> list = new ArrayList<>();
+        Runnable runnable = () -> list.add(3);
+        FunctionalUtils.add(3, runnable);
+        assertThat(list, is(Arrays.asList(3)));
+    }
+}
